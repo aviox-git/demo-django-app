6020f355a854a6bb842093f82b44ad0fdd4949e5
rajinder mohan <rajinder@avioxtechnologies.com.com>
2024-11-27 00:43:46 +0530

    fix changes

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('save-person/<int:pk>/', views.save_person, name='edit_person'),
     path('save-person/', views.save_person, name='add_person'),
