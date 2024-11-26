f915da8018149fc169d5c3983606f1618ff40d5e
rajinder mohan <rajinder@avioxtechnologies.com.com>
2024-11-27 00:30:32 +0530

    add fixes

--- demoproject/urls.py
+++ demoproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for demo project.
+URL configuration for demoproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
